444e13bd6e533edab4a19bc37db38ef4a5c3c630
“Jona <volohoj@gmail.com>
2026-05-17 11:56:48 +0200

    renamed sft

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .data_preprocess import pre_process 
-from .ar.run_training import run_training
+from .ar.run_training_sft import run_training
 from .data_posprocess import parse_generated_response, parse_content
 
 __all__ = ['pre_process', 'run_training', 'parse_generated_response', 'parse_content', 'run_training']  
